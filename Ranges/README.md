0be606d01250ba04e76d69e04d161ac81ed355fd
BytesOnTheRocks <bytesontherocks@gmail.com>
2024-06-18 17:55:12 +0100

    working on ranges

---
+++ Ranges/README.md
@@ -0,0 +1,3 @@
+Playing with templates
+
+- specialization
---
+++ Ranges/src/main.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <ranges>
+#include <iostream>
+
+int main()
+{
+  {
+    // piping outputs
+    //
+    std::vector<int> ints{0,1,2,3,4,5};
+    auto even = [](int i){ return 0 == i % 2; };
+    auto square = [](int i) { return i * i; };
+
+    for (int i : ints | std::views::filter(even) | std::views::transform(square)) {
+      std::cout << i << ' ';
+    }
+  }
+
+  {
+    // braking down the solution
+    //
+    std::vector<int> ints{0,1,2,3,4,5};
+    auto even = [](int i){ return 0 == i % 2; };
+    auto square = [](int i) { return i * i; };
+
+    auto ve = std::views::filter(ints, even);
+    
+    std::cout << std::endl;
+
+    for (const auto& e : ve)
+    {
+      std::cout << e << ' ';
+    }
+
+    auto sq = std::views::transform(ve, square);
+
+    std::cout << std::endl;
+
+    for (const auto& s : sq)
+    {
+      std::cout << s << ' ';
+    }
+
+  }
+}
